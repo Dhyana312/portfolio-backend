610ef1c05cf0c9c8315de220a1dd77ab57f54ba2
Dhyana Dave <dhyanadave312@gmail.com>
2026-03-03 10:13:45 +0530

    fix port for backend

--- server.js
+++ server.js
@@ -29,8 +29,13 @@ app.use("/api", require("./routes/contact.route"));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
-
+/* ---------------- ROOT ROUTE ---------------- */
+app.get("/", (req, res) => {
+  res.send("Portfolio Backend is Running 🚀");
+});
 /* ---------------- SERVER ---------------- */
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
